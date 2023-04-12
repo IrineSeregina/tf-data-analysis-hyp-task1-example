5225d73846e52d4b35bbf1aac5238145d0464191
IrineSeregina <73775364+IrineSeregina@users.noreply.github.com>
2023-04-12 20:53:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 432056961 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,11 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    
+    # конверсии в контроле и тесте соотв
+    conv_x = x_success / x_cnt 
+    conv_y = y_success / y_cnt
+    # uplift. считаем, что тест показал себя лучше. Если показывает себя лучше контроль, то ( conv_x / conv_y - 1 )
+    uplift = conv_y / conv_x - 1
+    
     return ... # Ваш ответ, True или False
